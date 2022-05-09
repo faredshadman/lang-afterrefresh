cdaaee7c292bf24cf030a70641edaab0634854d4
Fərid <feridhelios@gmail.com>
2022-05-10 00:09:18 +0400

    lang-refresh

---
+++ src/components/LocaleSwitcher.vue
@@ -0,0 +1,24 @@
+<template>
+  <select v-model="$i18n.locale" @change="handleChange($i18n.locale)">
+    <option v-for="(locale, i) in locales" :key="`locale-${i}`" :value="locale">
+      {{ locale }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "LocaleSwitcher",
+  data() {
+    return { locales: ["az", "en"] };
+  },
+  mounted() {
+    if (localStorage.Lang != null) this.$i18n.locale = localStorage.Lang;
+  },
+  methods: {
+    handleChange(lang) {
+      localStorage.Lang = lang;
+    },
+  },
+};
+</script>
